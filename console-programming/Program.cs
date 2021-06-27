3c9b30dd52bea0216da286a9b602fcbabd7f598f
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2021-06-27 13:59:09 +0300

    first commit

---
+++ console-programming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your Surname:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname);
+            
+        }
+    }
+}
