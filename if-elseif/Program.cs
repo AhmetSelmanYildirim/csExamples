e467c47ccc9eebec6427ea39d166e14f793a3e01
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2021-07-01 20:32:15 +0300

    if-elseif added

---
+++ if-elseif/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace if_elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+
+            if (time < 12 && time >= 6)
+            {
+                System.Console.WriteLine("Good morning");
+            }
+            else if (time <= 18 && time >= 12)
+            {
+                System.Console.WriteLine("Good afternoon");
+            }
+            else
+            {
+                System.Console.WriteLine("Good evening");
+            }
+
+            string result = time <= 18 ? "Good afternoon" : "Good evening";
+
+            result = time < 12 && time >= 6 ? "Good morning" : time <= 18 && time >= 12 ? "Good afternoon" : "Good evening";
+
+            System.Console.WriteLine(result);
+
+        }
+    }
+}
