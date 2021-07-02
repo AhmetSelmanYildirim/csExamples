8405ff59ee89fc480d4db678b2a51a7258a84341
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2021-07-02 19:22:14 +0300

    while-foreach added

---
+++ while-foreach-loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace while_foreach_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            int num = int.Parse(Console.ReadLine());
+            int counter = 1;
+            int sum = 0;
+
+            while (counter <= num)
+            {
+                sum += counter;
+                counter++;
+            }
+            Console.WriteLine(sum / num);
+
+            char c = 'a';
+            while (c <= 'z')
+            {
+                Console.Write(c);
+                c++;
+            }
+            System.Console.WriteLine("\n*****Foreach*****");
+            string[] cars = { "BMW", "Ford", "VW", "Mercedes" };
+
+            foreach (var car in cars)
+            {
+                System.Console.WriteLine(car);
+            }
+
+        }
+    }
+}
