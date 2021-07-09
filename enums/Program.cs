8448f781a493b4e7bf86d76e60ac75063d3a57c3
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2021-07-09 19:37:20 +0300

    enums added

---
+++ enums/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Days.Sunday);
+            Console.WriteLine((int)Days.Saturday);
+
+            int temp = 32;
+            if (temp <= (int)Weather.Normal)
+            {
+                Console.WriteLine("It's cold to go outside");
+            }
+            else if (temp >= (int)Weather.Hot)
+            {
+                Console.WriteLine("It's so hot");
+            }
+            else if (temp >= (int)Weather.Normal && temp < (int)Weather.VeryHot)
+            {
+                Console.WriteLine("Let's go to outside");
+            }
+
+        }
+    }
+
+    enum Days
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday = 23,
+        Saturday,
+        Sunday,
+
+    }
+
+    enum Weather
+    {
+        Cold = 5,
+        Normal = 20,
+        Hot = 25,
+        VeryHot = 30,
+    }
+
+}
