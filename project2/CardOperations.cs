0865b5c2e582ceaef90b73e39497f721a5c2fa14
AhmetSelmanYildirim <ahmet.selman.yildirimm@gmail.com>
2021-11-16 14:18:24 +0300

    project2 added

---
+++ project2/CardOperations.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    class CardOperations
+    {
+        public static void listTheBoard(List<Card> mainList)
+        {
+            Console.WriteLine("TODO Line \n*******************");
+            mainList.ForEach(i =>
+            {
+                if (i.Line == Line.TODO)
+                {
+                    Console.WriteLine("Title       : " + i.Title);
+                    Console.WriteLine("Description : " + i.Description);
+                    Console.WriteLine("Member      : " + i.Member.Name);
+                    Console.WriteLine("Size        : " + i.Size);
+                    Console.WriteLine("Line        : " + i.Line);
+                    Console.WriteLine("-");
+                }
+
+            });
+            Console.WriteLine("\nIN PROGRESS Line \n*******************");
+            mainList.ForEach(i =>
+            {
+                if (i.Line == Line.IN_PROGRESS)
+                {
+                    Console.WriteLine("Title       : " + i.Title);
+                    Console.WriteLine("Description : " + i.Description);
+                    Console.WriteLine("Member      : " + i.Member.Name);
+                    Console.WriteLine("Size        : " + i.Size);
+                    Console.WriteLine("Line        : " + i.Line);
+                    Console.WriteLine("-");
+                }
+
+            });
+            Console.WriteLine("\nDONE Line \n*******************");
+            mainList.ForEach(i =>
+            {
+                if (i.Line == Line.DONE)
+                {
+                    Console.WriteLine("Title       : " + i.Title);
+                    Console.WriteLine("Description : " + i.Description);
+                    Console.WriteLine("Member      : " + i.Member.Name);
+                    Console.WriteLine("Size        : " + i.Size);
+                    Console.WriteLine("Line        : " + i.Line);
+                    Console.WriteLine("-");
+                }
+
+            });
+        }
+
+        public static void addCard(List<Member> members, List<Card> mainList)
+        {
+            try
+            {
+                Console.Write("Title: ");
+                string title = Console.ReadLine();
+                Console.Write("Description: ");
+                string description = Console.ReadLine();
+
+                // Get member
+                Console.WriteLine("Member: ");
+                Console.WriteLine("(1) Ahmet");
+                Console.WriteLine("(2) Murat");
+                Console.WriteLine("(3) Batuhan");
+                Console.WriteLine("Default = Ahmet");
+                var selection = int.Parse(Console.ReadLine());
+                Member selectedMember = members[0];
+                switch (selection)
+                {
+                    case 1:
+                        selectedMember = members[0];
+                        break;
+                    case 2:
+                        selectedMember = members[1];
+                        break;
+                    case 3:
+                        selectedMember = members[2];
+                        break;
+                    default: Console.WriteLine("Incorrect value entered."); break;
+                }
+
+                // Get size
+                Console.WriteLine("Size: ");
+                Console.WriteLine("1: XS");
+                Console.WriteLine("2: S");
+                Console.WriteLine("3: M");
+                Console.WriteLine("4: L");
+                Console.WriteLine("5: XL");
+                Console.WriteLine("Default = XS");
+                selection = int.Parse(Console.ReadLine());
+                Size size = Size.XS;
+                switch (selection)
+                {
+                    case 1: size = Size.XS; break;
+                    case 2: size = Size.S; break;
+                    case 3: size = Size.M; break;
+                    case 4: size = Size.L; break;
+                    case 5: size = Size.XL; break;
+                    default: Console.WriteLine("Do not try me, please."); break;
+                }
+
+                // Get line
+                Console.WriteLine("Line: ");
+                Console.WriteLine("1: TODO");
+                Console.WriteLine("2: IN PROGRESS");
+                Console.WriteLine("3: DONE");
+                Console.WriteLine("Default = TODO");
+                selection = int.Parse(Console.ReadLine());
+                Line line = Line.TODO;
+
+                switch (selection)
+                {
+                    case 1: line = Line.TODO; break;
+                    case 2: line = Line.IN_PROGRESS; break;
+                    case 3: line = Line.DONE; break;
+                    default: Console.WriteLine("Do not try me, please."); break;
+                }
+
+
+                mainList.Add(new Card(title, description, selectedMember, size, line));
+
+            }
+            catch (FormatException e)
+            {
+                System.Console.WriteLine("Data type incorrect");
+            }
+            
+        }
+
+        public static void deleteCard(List<Card> mainList)
+        {
+            try
+            {
+                Console.WriteLine("Write title of card you want to delete.");
+                Console.Write("Title: ");
+                string titleToDelete = Console.ReadLine();
+                int indexTitleToDelete = mainList.FindIndex(r => r.Title == titleToDelete);
+
+                if (indexTitleToDelete == -1)
+                {
+                    Console.WriteLine("Cannot find the card");
+                }
+                else
+                {
+                    mainList.RemoveAt(indexTitleToDelete);
+                }
+            }
+            catch (FormatException e)
+            {
+                System.Console.WriteLine("Data type incorrect");
+            }
+
+        }
+
+        public static void updateCard(List<Card> mainList)
+        {
+            try
+            {
+                Console.WriteLine("Write title of card you want to update.");
+                Console.Write("Title: ");
+                string titleToUpdate = Console.ReadLine();
+                int indexTitleToUpdate = mainList.FindIndex(r => r.Title == titleToUpdate);
+
+                if (indexTitleToUpdate == -1)
+                {
+                    Console.WriteLine("Cannot find the card");
+                }
+                else
+                {
+                    Console.WriteLine("Card details: \n*****************");
+                    Console.WriteLine("Title       : " + mainList[indexTitleToUpdate].Title);
+                    Console.WriteLine("Description : " + mainList[indexTitleToUpdate].Description);
+                    Console.WriteLine("Member      : " + mainList[indexTitleToUpdate].Member.Name);
+                    Console.WriteLine("Size        : " + mainList[indexTitleToUpdate].Size);
+                    Console.WriteLine("Line        : " + mainList[indexTitleToUpdate].Line);
+                    Console.WriteLine("\nPlease choose the line you want card to move: ");
+                    Console.WriteLine("(1) TODO");
+                    Console.WriteLine("(2) IN PROGRESS");
+                    Console.WriteLine("(3) DONE");
+                    int selectedLine = int.Parse(Console.ReadLine());
+                    if (selectedLine == 1) { mainList[indexTitleToUpdate].Line = Line.TODO; CardOperations.listTheBoard(mainList); }
+                    else if (selectedLine == 2) { mainList[indexTitleToUpdate].Line = Line.IN_PROGRESS; CardOperations.listTheBoard(mainList); }
+                    else if (selectedLine == 3) { mainList[indexTitleToUpdate].Line = Line.DONE; CardOperations.listTheBoard(mainList); }
+                    else { Console.WriteLine("Incorrect value entered."); }
+
+                }
+            }
+            catch (FormatException e)
+            {
+                System.Console.WriteLine("Data type incorrect");
+            }
+
+            
+        }
+    }
+}
---
+++ project2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Member list
+            List<Member> members = new()
+            {
+                new Member(1, "Ahmet"),
+                new Member(2, "Murat"),
+                new Member(3, "Batuhan")
+            };
+
+            List<Card> mainList = new();
+
+            mainList.Add(new Card("Ekmek al", "Marketten 2 ekmek al", members[0], Size.L, Line.TODO));
+            mainList.Add(new Card("Kitap oku", "50 sayfa kitap oku", members[1], Size.M, Line.IN_PROGRESS));
+            mainList.Add(new Card("Odayı topla", "Eşyaları yerine koy", members[2], Size.XL, Line.DONE));
+
+            int q = 1;
+            while (q == 1)
+            {
+                Console.WriteLine("\nChoose");
+                Console.WriteLine("(1) List Board ");
+                Console.WriteLine("(2) Add Card ");
+                Console.WriteLine("(3) Delete Card");
+                Console.WriteLine("(4) Replace Card");
+                Console.WriteLine("(5) Quit");
+                string choose = Console.ReadLine();
+
+                try
+                {
+                    switch (int.Parse(choose))
+                    {
+                        // Listing
+                        case 1: CardOperations.listTheBoard(mainList); break;
+
+                        // Adding
+                        case 2: CardOperations.addCard(members, mainList); break;
+
+                        case 3: CardOperations.deleteCard(mainList); break;
+
+                        case 4: CardOperations.updateCard(mainList); break;
+
+                        case 5: q = 0; break;
+
+                        default: Console.WriteLine("Do not try me, please."); break;
+                    }
+                }
+                catch (FormatException e)
+                {
+                    System.Console.WriteLine("Data type incorrect");
+                }
+
+            }
+
+
+
+        }
+    }
+}
